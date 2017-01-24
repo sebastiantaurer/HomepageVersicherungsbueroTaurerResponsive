ebf5e22893717344ecbdba5ef03f5cbeefd7aa86
sebastiantaurer <sebastiantaurer@gmail.com>
2017-01-24 09:40:21 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="col-sm-2">
+	<img src="img/TMLogo300x300.png" height="150" width="150"/>
+</div>
+<div class="col-sm-10">
+	<h1>Versicherungs- & Finanzdienstleistungs GmbH</h1>
+	<h2>Michael Taurer</h2>
+	<h3>Rufen Sie uns unverbindlich an 04762/33880</h3> 
+</div>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--***** CSS *****-->
+		<!-- bootstrap CSS -->
+		<link href="libs/bootstrap/dist/css/bootstrap.css" rel="stylesheet" />
+		<!-- Main CSS -->
+		<link href="css/main.css" rel="stylesheet" />
+		<!--***** JS *****-->
+		<!-- jquery JS -->
+		<script src="libs/jquery/jquery.js" type="text/javascript"></script>
+		<!-- bootstrap JS -->
+		<script src="libs/bootstrap/dist/js/bootstrap.js" type="text/javascript"></script>
+		<!-- main JS -->
+		<script src="js/main.js" type="text/javascript"></script>
+		<title>Versicherungsb&uuml;ro Taurer</title>
+	</head>
+	<body>
+		<div class="container content">
+			<!--HEADER-->
+			<div class="row header">
+				<?php include ("header.php"); ?>
+			</div>
+			<hr>
+			<!--NAVIGATION-->
+			<div class="row">
+			
+			</div>
+			<!--MAIN_AREA-->
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Willkommen!</h1>
+				</div>
+			</div>
+			<!--FOOTER-->
+			<div class="row">
+			</div>
+		</div>
+	</body>
+</html>
