a66d81f1a509b41b13ddf72b9b8213103e745671
Sebastian Taurer <sebastiantaurer@gmail.com>
2020-07-06 21:31:33 +0200

    updated team page

--- team.php
+++ team.php
@@ -39,8 +39,8 @@
 									<h3>Akademischer Finanzdienstleister</h3>
 									<h3>Staatlich gepr. Versicherungsmakler</h3>
 									<h3>Versicherungsagent</h3>
-									<p>Tel.: 04762/33 880</p>
-									<p>Mobil: 0699/18 79 54 27</p>
+									<p>Tel.: <a href="tel:+43476233880">04762/33 880</a></p>
+									<p>Mobil: <a href="tel:+4369918795427">0699/18 79 54 27</a></p>
 									<p>E-Mail: <a href="mailto:michael.taurer@taurer.eu">michael.taurer@taurer.eu</a></p>
 								</div>
 							</div>
@@ -55,23 +55,37 @@
 									<h1>Frau Karoline Taurer</h1>
 									<h2>Kundenbetreuung</h2>
 									<h3>Staatlich gepr. Versicherungsmaklerin</h3>
-									<p>Tel.: 04762/33 880</p>
-									<p>Mobil: 0660/654 9242</p>
+									<p>Tel.: <a href="tel:+43476233880">04762/33 880</a></p>
+									<p>Mobil: <a href="tel:+436606549242">0660/654 9242</a></p>
 									<p>E-Mail: <a href="mailto:karoline.taurer@taurer.eu">karoline.taurer@taurer.eu</a></p>
 								</div>
 							</div>
 							<br/>
-							<!--Eva Madritsch-->
+							<!--Sandra Perauer-->
 							<div class="row">
 								<div class="col-sm-1"><br/></div>
 								<div class="col-sm-3">
-									<img class="modalImage" alt="Eva Madritsch" src="img/evamadritsch.jpg"/>
+									<img class="modalImage" alt="Sandra Perauer" src="img/anonymuswhite.png"/>
 								</div>
 								<div class="col-sm-8">
-									<h1>Frau Eva Madritsch</h1>
-									<h2>Kundenbetreuung</h2>
-									<p>Tel.: 04762/33 880</p>
-									<p>E-Mail: <a href="mailto:eva.madritsch@taurer.eu">eva.madritsch@taurer.eu</a></p>
+									<h1>Frau Sandra Perauer</h1>
+									<h2>Backoffice</h2>
+									<p>Tel.: <a href="tel:+43476233880">04762/33 880</a></p>
+									<p>E-Mail: <a href="mailto:sandra.perauer@taurer.eu">sandra.perauer@taurer.eu</a></p>
+								</div>
+							</div>
+							<br/>
+							<!--Michelle Berger-->
+							<div class="row">
+								<div class="col-sm-1"><br/></div>
+								<div class="col-sm-3">
+									<img class="modalImage" alt="Michelle Berger" src="img/anonymuswhite.png"/>
+								</div>
+								<div class="col-sm-8">
+									<h1>Frau Michelle Berger</h1>
+									<h2>Backoffice</h2>
+									<p>Tel.: <a href="tel:+43476233880">04762/33 880</a></p>
+									<p>E-Mail: <a href="mailto:michelle.berger@taurer.eu">michelle.berger@taurer.eu</a></p>
 								</div>
 							</div>
 							<br/>
@@ -84,8 +98,7 @@
 								<div class="col-sm-8">
 									<h1>Herr Sebastian Taurer</h1>
 									<h2>IT Administrator</h2>
-									<p>Tel.: 04762/33 880</p>
-									<p>E-Mail: setaurer@edu.aau.at</p>
+									<p>Tel.: <a href="tel:+43476233880">04762/33 880</a></p>
 								</div>
 							</div>
 							<br/>
